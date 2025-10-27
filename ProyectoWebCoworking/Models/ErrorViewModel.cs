2d4047dafe1409923da4ecf1368fde0a03e517d8
davidsored <davidsored@gmail.com>
2025-10-27 18:13:08 +0100

    Agregar archivos de proyecto.

---
+++ ProyectoWebCoworking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoWebCoworking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
