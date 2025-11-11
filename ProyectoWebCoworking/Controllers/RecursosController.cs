600d1c973413cfed1ecbdbf56515fbc3609973fa
davidsored <davidsored@gmail.com>
2025-11-11 23:29:35 +0100

    Implementación del CRUD completo de Recursos con modal de eliminación

--- ProyectoWebCoworking/Controllers/RecursosController.cs
+++ ProyectoWebCoworking/Controllers/RecursosController.cs
@@ -16,6 +16,8 @@ namespace ProyectoWebCoworking.Controllers
             _context = context;
         }
 
+        #region Index
+
         //La acción Index será la página principal y mostrará la lista de recursos
         public IActionResult Index()
         {
@@ -24,5 +26,98 @@ namespace ProyectoWebCoworking.Controllers
             
             return View(listaRecursos);
         }
+
+        #endregion
+
+        #region Create
+        //Establecemos el formulario para crear un nuevo recurso con los métodos Get para mostrarlo y Post para guardarlo
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();        
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Nombre, Tipo, Capacidad")] Recurso recurso)
+        {
+            //Comprobamos si los datos recibidos son válidos, añadimos el recurso a la base de datos y redirigimos al usuario a la vista
+            if (ModelState.IsValid)
+            {
+                _context.Recursos.Add(recurso);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(recurso);
+        }
+
+        #endregion
+
+        #region Edit
+        //Esta acción busca un recurso por su Id y muestra el formulario para editarlo
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            //Validamos el Id
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            //Buscamos el recurso en la base de datos por su Id y comprobamos si existe
+            var recurso = _context.Recursos.Find(id);
+            if(recurso == null)
+            {
+                return NotFound();
+            }
+
+            return View(recurso);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id, Nombre, Tipo, Capacidad")] Recurso recurso)
+        {
+            //Comprobamos que el Id de la URL coincide con el Id del formulario
+            if (id != recurso.Id)
+            {
+                return NotFound();
+            }
+
+            //Comprobamos si los datos son válidos como en el Edit
+            if (ModelState.IsValid)
+            {
+                _context.Update(recurso);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(recurso);
+        }
+
+        #endregion
+
+        #region Delete
+               
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        //Llamamos DeleteConfirmed al método para que no entre en conflicto con el método Get
+        public IActionResult DeleteConfirmado(int id)
+        {
+            //Buscamos el recurso y si existe, se borra
+            var recurso = _context.Recursos.Find(id);
+            if (recurso != null)
+            {
+                _context.Recursos.Remove(recurso);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+            #endregion
     }
 }
